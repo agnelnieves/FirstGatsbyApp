0100da0289cd3ab365dd3c5a16cebea67f1f866d
Agnel Nieves <agnelnieves@gmail.com>
2018-09-12 17:15:53 -0400

    Merged with development

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World!</h1>
+    <p>This is my first take on React</p>
+    <h3>Technologies used:</h3>
+    <ul>
+      <li>React Js</li>
+      <li>Webpack</li>
+      <li>GraphQL</li>
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
