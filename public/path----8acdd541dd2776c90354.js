fd442450b2c1acc14d8a3e35a1eadf6a7972da9c
Agnel Nieves <agnelnieves@gmail.com>
2018-09-17 11:33:50 -0400

    removed public

--- public/path----8acdd541dd2776c90354.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{106:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Girst take on Gatsby"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----8acdd541dd2776c90354.js.map
